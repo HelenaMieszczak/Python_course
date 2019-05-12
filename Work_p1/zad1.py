24e56704817c93f5b4c18b0b5a9ad2766f99476a
HelenaMieszczak <50418618+HelenaMieszczak@users.noreply.github.com>
2019-05-12 12:18:18 +0200

    Add files via upload

--- Work_p1/zad1.py
+++ Work_p1/zad1.py
@@ -1,1 +1,4 @@
+# Zadanie 1
+# Napisz skrypt, który oblicza ile minut ma rok
 
+print("W roku jest", 365*24*60, "minut.")
---
+++ Work_p1/zad4.py
@@ -0,0 +1,13 @@
+# Zadanie 4
+# Napisz skrypt, który zapyta użytkownika o trzy liczby całkowite,
+# a następnie pomnóż dwa pierwsze. przed podzieleniem wyniku przez trzecią liczbę. 
+# Wejście i wyjście powinny być zrozumiałe dla użytkownika.
+
+
+print("Poniżej obliczony zostanie wynik działania: a*b/c. Podaj 3 dowolne liczby całkowite:")
+
+number1 = int(input("a = "))
+number2 = int(input("b = "))
+number3 = int(input("c = "))
+
+print("Wynik to: ", number1 * number2 / number3)
