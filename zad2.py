1d3e2b103e939a08a03220ef89be48a23da613e1
HelenaMieszczak <50418618+HelenaMieszczak@users.noreply.github.com>
2019-05-12 12:27:50 +0200

    Delete zad2.py

--- zad2.py
+++
@@ -1,8 +0,0 @@
-# Zadanie 2
-# Napisz skrypt, który pobiera dwie wiadomości od użytkownika
-# a następnie wyświetla je na ekranie poprzedzone ostrzeżeniem "UWAGA: ..." ​
-
-message1 = input("Podaj nazwę kursu: ")
-message2 = input("Podaj datę i godzinę zajęć: ")
-
-print("Uwaga:", message1, message2)
